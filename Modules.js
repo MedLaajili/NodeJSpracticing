199563afeedb6745af8cd29e171f2126885a5a75
MedLaajili <mohamed.laajili@polytechnicien.tn>
2022-04-09 13:04:15 +0100

    first commit

---
+++ Modules.js
@@ -0,0 +1,6 @@
+const xyz = require('./people');
+
+console.log(xyz.ages[3]);
+
+const os = require('os');
+console.log(os.platform(), os.homedir());
---
+++ people.js
@@ -0,0 +1,8 @@
+const people=['mohamed','emna','sirine','noura','hakim'];
+const ages=[24,30,27,48,56];
+
+console.log(people);
+
+module.exports = {
+    people, ages
+};
