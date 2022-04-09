805ca773b2152bed0a80974e549e01ad8ff2a5a4
MedLaajili <mohamed.laajili@polytechnicien.tn>
2022-04-09 13:48:16 +0100

    streams and buffer

--- streams.js
+++ streams.js
@@ -1,1 +1,23 @@
 const fs = require('fs');
+
+const readStream = fs.createReadStream('./docs/blog.txt',{encoding:'utf8'});
+const writeStream = fs.createWriteStream('./docs/blog3.txt');
+
+/*
+readStream.on('data',(chunk)=>{
+    console.log('----New chunk----');
+    //console.log(chunk);
+    writeStream.write('\nNew chunk\n');
+    writeStream.write(chunk);
+});
+*/
+
+//piping
+//readStream.pipe(writeStream);
+
+if(fs.existsSync('./docs/blog3.txt')){
+    fs.unlink('./docs/blog3.txt',()=>console.log('file deleted'));
+}else{
+    readStream.pipe(writeStream);
+    console.log('file created'); 
+}
