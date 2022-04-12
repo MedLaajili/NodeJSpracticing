8565a2707724f8b2a282c3bc7b23c9db3e935061
MedLaajili <mohamed.laajili@polytechnicien.tn>
2022-04-12 15:01:10 +0100

    some lodash method

--- server/server.js
+++ server/server.js
@@ -3,8 +3,17 @@ const fs = require('fs');
 const _ = require('lodash');
 
 const server = http.createServer((req,res)=>{
-    console.log(req.url, req.method);
     
+    // Lodash
+    const num = _.random(0,20);
+    console.log(num);
+
+    const greet= _.once(()=>{
+        console.log('hello');
+    })
+
+    greet();
+    greet();
     /*set header content type
     res.setHeader('Content-type','text/html');
     res.write('<head><title>MyPage</title></head>')
