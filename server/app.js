4213469d8e4096df99fecf25c90f8d6a78f655ad
MedLaajili <mohamed.laajili@polytechnicien.tn>
2022-04-14 14:13:22 +0100

    passing data through EJS

--- server/app.js
+++ server/app.js
@@ -3,21 +3,30 @@ const express = require('express');
 // express app
 const app = express();
 
+//Register view engine
+app.set('view engine','ejs');
+
 //listen for requests
 app.listen(3000);
 
 app.get('/',(req,res)=>{
-    res.sendFile('./views/index.html', {root:__dirname});
+    const blogs = [
+        {title:'Rich dad poor dad', snippet:'lorem ipsum dolor sit amet consectetur'},
+        {title:'The Atomic Habits', snippet:'lorem ipsum dolor sit amet consectetur'},
+        {title:'the Moneyball', snippet:'lorem ipsum dolor sit amet consectetur'}
+    ];
+    const message= "there's no available blogs right now"
+    res.render('index',{title:'Home',blogs,message});
 });
 app.get('/about',(req,res)=>{
-    res.sendFile('./views/about.html',{root:__dirname});
+    res.render('about',{title:'About'});
 });
 
 // redirects
-app.get('/about-us',(req,res)=>{
-    res.redirect('/about');
+app.get('/blogs/create',(req,res)=>{
+    res.render('create',{title:'New Blog'});
 });
 //404 page
 app.use((req,res)=>{
-    res.status(404).sendFile('./views/404.html',{root:__dirname});
+    res.status(404).render('404',{title:'Error'});
 });
